ad9d68203a0f8fe240c6a761327cc482dc0fcede
yuque <>
2021-11-26 08:22:58 +0800

    init

---
+++ TabBar/TabBar.py
@@ -0,0 +1,6 @@
+﻿# This Python file uses the following encoding: utf-8
+from PyQt5 import QtWidgets
+
+class TabBar(QtWidgets.QFrame):
+    def __init__(self):
+        pass
---
+++ TabPane/TabPane.py
@@ -0,0 +1,7 @@
+﻿# This Python file uses the following encoding: utf-8
+from PyQt5 import QtWidgets
+
+
+class TabPane(QtWidgets.QFrame):
+    def __init__(self):
+        pass
