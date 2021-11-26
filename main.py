61b451562ec06b9122b0499ade289060a22ae2be
yuque <lihuaibin@live.com>
2021-11-26 21:25:53 +0800

    复制PySideQMLToyBox项目

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿# This Python file uses the following encoding: utf-8
+import sys
+from PyQt5.QtWidgets import QApplication
+from MainWindow import MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.showMaximized()
+    sys.exit(app.exec_())
