66e856ba6266aaf47ef4d89d183e7f4c8bc78071
yuque <lihuaibin@live.com>
2021-11-30 23:01:43 +0800

    解决未更新时崩溃问题

--- Covid19HttpHelper/Covid19HttpHelper.py
+++ Covid19HttpHelper/Covid19HttpHelper.py
@@ -13,89 +13,89 @@ class Covid19HttpHelper(QtCore.QObject):
     def getChartsData(self):
         r = requests.get(self.__url, headers=self.__header)
         jsonObject = r.json()
-        msg = jsonObject["msg"]
+        msg = jsonObject.get("msg")
         if msg != "成功":
             return ""
 
-        chinaDayList = jsonObject["data"]["chinaDayList"]
+        chinaDayList = jsonObject.get("data").get("chinaDayList")
         todayArray = [["date", "确诊", "疑似", "输入"]]
         totalArray = [["date", "累计确诊", "现有确诊"]]
 
         for i in range(len(chinaDayList)):
-            today = chinaDayList[i]["today"]
+            today = chinaDayList[i].get("today")
             todayInfo = []
-            todayInfo.append(chinaDayList[i]["date"])
-            todayInfo.append(today["confirm"])
-            todayInfo.append(today["suspect"])
-            todayInfo.append(today["input"])
+            todayInfo.append(chinaDayList[i].get("date"))
+            todayInfo.append(today.get("confirm"))
+            todayInfo.append(today.get("suspect"))
+            todayInfo.append(today.get("input"))
             todayArray.append(todayInfo)
 
-            total = chinaDayList[i]["total"]
+            total = chinaDayList[i].get("total")
             totalInfo = []
-            totalInfo.append(chinaDayList[i]["date"])
-            totalInfo.append(total["confirm"])
-            totalInfo.append(total["storeConfirm"])
+            totalInfo.append(chinaDayList[i].get("date"))
+            totalInfo.append(total.get("confirm"))
+            totalInfo.append(total.get("storeConfirm"))
             totalArray.append(totalInfo)
 
-        updateTime = jsonObject["data"]["lastUpdateTime"]
+        updateTime = jsonObject.get("data").get("lastUpdateTime")
         return [json.dumps(todayArray), json.dumps(totalArray), updateTime]
 
     def getMapData(self):
         r = requests.get(self.__url, headers=self.__header)
         jsonObject = r.json()
-        msg = jsonObject["msg"]
+        msg = jsonObject.get("msg")
         if msg != "成功":
             return ""
 
-        areaTree = jsonObject["data"]["areaTree"]
+        areaTree = jsonObject.get("data").get("areaTree")
         chinaDailyData = []
         for i in range(len(areaTree)):
             countryObject = areaTree[i]
-            if countryObject["name"] != "中国":
+            if countryObject.get("name") != "中国":
                 continue
-            for j in range(len(countryObject["children"])):
-                provinceObject = countryObject["children"][j]
-                provinceTotal = provinceObject["total"]
+            for j in range(len(countryObject.get("children"))):
+                provinceObject = countryObject.get("children")[j]
+                provinceTotal = provinceObject.get("total")
                 province = {}
-                province["name"] = provinceObject["name"]
-                province["value"] = [provinceTotal["confirm"],
-                                provinceTotal["suspect"],
-                                provinceTotal["heal"],
-                                provinceTotal["dead"]]
+                province["name"] = provinceObject.get("name")
+                province["value"] = [provinceTotal.get("confirm"),
+                                provinceTotal.get("suspect"),
+                                provinceTotal.get("heal"),
+                                provinceTotal.get("dead")]
                 chinaDailyData.append(province)
 
-        updateTime = jsonObject["data"]["lastUpdateTime"]
+        updateTime = jsonObject.get("data").get("lastUpdateTime")
         return [json.dumps(chinaDailyData), updateTime]
 
     def getGeneralView(self):
         r = requests.get(self.__url, headers=self.__header)
         jsonObject = r.json()
-        msg = jsonObject["msg"]
+        msg = jsonObject.get("msg")
         if msg != "成功":
             return ""
 
         generalViewDatas = []
-        totalObject = jsonObject["data"]["chinaTotal"]["total"]
-        todayObject = jsonObject["data"]["chinaTotal"]["today"]
+        totalObject = jsonObject.get("data").get("chinaTotal").get("total")
+        todayObject = jsonObject.get("data").get("chinaTotal").get("today")
 
-        inputData = (totalObject["input"], todayObject["input"])
+        inputData = (totalObject.get("input", 0), todayObject.get("input", 0))
         generalViewDatas.append(inputData)
 
-        extDataObject = jsonObject["data"]["chinaTotal"]["extData"]
-        noSymptomData = (extDataObject["noSymptom"], extDataObject["incrNoSymptom"])
+        extDataObject = jsonObject.get("data").get("chinaTotal").get("extData")
+        noSymptomData = (extDataObject.get("noSymptom", 0), extDataObject.get("incrNoSymptom", 0))
         generalViewDatas.append(noSymptomData)
 
-        totalConfirm = totalObject["confirm"]
-        confirmData = (totalConfirm, todayObject["confirm"])
+        totalConfirm = totalObject.get("confirm", 0)
+        confirmData = (totalConfirm, todayObject.get("confirm", 0))
 
-        totalDead = totalObject["dead"]
-        deadData = (totalDead, todayObject["dead"])
+        totalDead = totalObject.get("dead", 0)
+        deadData = (totalDead, todayObject.get("dead", 0))
 
-        totalHeal = totalObject["heal"]
-        healData = (totalHeal, todayObject["heal"])
+        totalHeal = totalObject.get("heal", 0)
+        healData = (totalHeal, todayObject.get("heal", 0))
 
         storeConfirm = totalConfirm - totalDead - totalHeal
-        storeConfirmData = (storeConfirm, todayObject["storeConfirm"])
+        storeConfirmData = (storeConfirm, todayObject.get("storeConfirm", 0))
         generalViewDatas.append(storeConfirmData)
         generalViewDatas.append(confirmData)
         generalViewDatas.append(deadData)
